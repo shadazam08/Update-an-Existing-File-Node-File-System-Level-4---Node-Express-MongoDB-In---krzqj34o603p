6ae7fcc323d63c9cae320b81d737de18a42c520c
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-04-19 20:46:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  return await fs.appendFile(fileName, fileContent);
   
 };
+updateFile(fileName, “\n, An online learning platform”);
 
 module.exports = updateFile;
